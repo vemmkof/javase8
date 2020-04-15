f609b2ba0ff5ad8d5e116d48ea4a8600946c212a
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-04-15 15:25:55 -0500

    initial commit

---
+++ src/main/java/pearson/programming/java/javase8/App.java
@@ -0,0 +1,15 @@
+package pearson.programming.java.javase8;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
+	public static void main(String[] args) {
+		LOGGER.info("Hello World!");
+	}
+}
---
+++ src/test/java/pearson/programming/java/javase8/AppTest.java
@@ -0,0 +1,18 @@
+package pearson.programming.java.javase8;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
